2250518b2e044612c5ccd2967a53e034cbd3e4ce
ASahithi2005 <adlasahithi8@gmail.com>
2025-03-09 12:27:12 +0530

    Initial commit - React project setup

---
+++ placementWeb/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import react from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     Hello
+    </>
+  )
+}
+
+export default App
