1b83d3f25a8f98f6c6853da79c74b3035d5f19a5
Varsha <srivarsha8625@gmail.com>
2025-03-10 15:26:41 +0530

    Timeline updated

--- placementWeb/src/App.jsx
+++ placementWeb/src/App.jsx
@@ -2,14 +2,21 @@ import { useState } from 'react'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import react from 'react'
 import './App.css'
+import Timeline from "./components/Timeline";
+
+/* function App() {
+  return (
+    
+  );
+}
+ */
 
 function App() {
   return (
-    <>
-     Hello world
-     this is the first page
-    </>
-  )
+    <div>
+    <Timeline />
+    </div>
+  );
 }
 
-export default App
+export default App;
