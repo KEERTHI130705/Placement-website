3583d93d638e768b21733561c6482a3c0fe0ea69
ASahithi2005 <adlasahithi8@gmail.com>
2025-05-14 08:19:17 +0530

    carousel and statistics

---
+++ src/Components/Statistics.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../Styles/Statistics.css';
+import img1 from '../assets/statistics/placement1.png';
+import img2 from '../assets/statistics/PercentageOFstu1.png';
+
+const Statistics = () => {
+  return (
+    <section className="stats-section" id="statistics">
+      <h1 className="stats-title"><b>Our Impact in Numbers</b></h1>
+      <div className="stats-container">
+          <img src={img1} alt="Stat 1" />
+          <img src={img2} alt="Stat 2" />
+      </div>
+    </section>
+  );
+};
+
+export default Statistics;
