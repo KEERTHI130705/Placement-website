75a9bf288aff5fb48cb99749bc5bd7a324e14227
Manognan <manognan2046@gmail.com>
2025-05-23 13:40:18 +0530

    optimised

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,43 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './Components/Header';
-import './App.css';
-import Tpo_info from './Components/Tpo_info'
-import MessagesSection from './Components/tpo_messages';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import "./App.css";
+import Tpo_info from "./Components/Tpo_info";
+import MessagesSection from "./Components/tpo_messages";
 import Timeline from "./Components/Timeline";
-import HomeBanner from './Components/Homebanner';
-import StudentLogin from './Components/StudentLogin';
-import RecruiterLogin from './Components/RecruiterLogin';
-import Footer from './Components/Footer';
-import RecruitersGrid from './Components/RecruitersGrid';
-import Statistics from './Components/Statistics';
+import HomeBanner from "./Components/Homebanner";
+import StudentLogin from "./Components/StudentLogin";
+import RecruiterLogin from "./Components/RecruiterLogin";
+import Footer from "./Components/Footer";
+import RecruitersGrid from "./Components/RecruitersGrid";
+import Statistics from "./Components/Statistics";
 
 function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={
-          <>
-            <HomeBanner />
-            <Tpo_info />
-            <MessagesSection />
-            <Statistics/>
-
-            <RecruitersGrid/>
-            <Timeline />
-            <Footer/>
-          </>
-        } />
-        {/* <Route path="/student-login" element={<StudentLogin />} />
+      <div className="content-wrapper">
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <HomeBanner />
+                <Tpo_info />
+                <MessagesSection />
+                <Statistics />
+                <RecruitersGrid />
+                <Timeline />
+                <Footer />
+              </>
+            }
+          />{" "}
+          {/* <Route path="/student-login" element={<StudentLogin />} />
         <Route path="/recruiter-login" element={<RecruiterLogin />} /> */}
-      </Routes>
+        </Routes>
+      </div>
     </Router>
   );
 }
 
 export default App;
-
