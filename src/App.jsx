cc5cb6fd8a1595263ea3402cdd783f04a0635664
KEERTHI130705 <alle.100523735001@gmail.com>
2025-06-04 19:34:57 +0530

    Update contact form card size and styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,65 +5,34 @@ import "./App.css";
 import Tpo_info from "./Components/Tpo_info";
 import MessagesSection from "./Components/tpo_messages";
 import Timeline from "./Components/Timeline";
-<<<<<<< HEAD
-import HomeBanner from './Components/Homebanner';
-import StudentLogin from './Components/StudentLogin';
-import RecruiterLogin from './Components/RecruiterLogin';
-import Footer from './Components/Footer';
-import RecruitersGrid from './Components/RecruitersGrid';
-import Statistics from './Components/Statistics';
-import ContactForm from './Components/contactform.jsx';
-=======
 import HomeBanner from "./Components/Homebanner";
 import StudentLogin from "./Components/StudentLogin";
 import RecruiterLogin from "./Components/RecruiterLogin";
 import Footer from "./Components/Footer";
 import RecruitersGrid from "./Components/RecruitersGrid";
 import Statistics from "./Components/Statistics";
+import ContactForm from "./Components/contactform.jsx";
 
->>>>>>> e995e62b4665ec98e5b952ed456d89c23578c065
 function App() {
   return (
     <Router>
       <Header />
-<<<<<<< HEAD
       <Routes>
         <Route path="/" element={
           <>
             <HomeBanner />
             <Tpo_info />
             <MessagesSection />
-            <Statistics/>
-
-            <RecruitersGrid/>
+            <Statistics />
+            <RecruitersGrid />
             <Timeline />
             <ContactForm />
-            <Footer/>
+            <Footer />
           </>
         } />
-        {/* <Route path="/student-login" element={<StudentLogin />} />
-=======
-      <div className="content-wrapper">
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <HomeBanner />
-                <Tpo_info />
-                <MessagesSection />
-                <Statistics />
-                <RecruitersGrid />
-                <Timeline />
-                <Footer />
-              </>
-            }
-          />{" "}
-          {/* <Route path="/student-login" element={<StudentLogin />} />
->>>>>>> e995e62b4665ec98e5b952ed456d89c23578c065
-        <Route path="/recruiter-login" element={<RecruiterLogin />} /> */}
-        </Routes>
-      </div>
+        {/* <Route path="/student-login" element={<StudentLogin />} /> */}
+        {/* <Route path="/recruiter-login" element={<RecruiterLogin />} /> */}
+      </Routes>
     </Router>
   );
 }
